11a551052344042a3d4f8bce58d3e738f91552b7
issa995 <issa.jafari95@gmail.com>
2026-07-16 15:38:14 +0300

    add vehicle DTOs and service contract

---
+++ src/VehicleFinder.Application/DTOs/VehicleMakeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VehicleFinder.Application.DTOs
+{
+    public sealed class VehicleMakeDto
+    {
+        public int MakeId { get; init; }
+
+        public string MakeName { get; init; } = string.Empty;
+    }
+}
---
+++ src/VehicleFinder.Application/DTOs/VehicleTypeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VehicleFinder.Application.DTOs
+{
+    public sealed class VehicleTypeDto
+    {
+        public int VehicleTypeId { get; init; }
+
+        public string VehicleTypeName { get; init; } = string.Empty;
+    }
+}
